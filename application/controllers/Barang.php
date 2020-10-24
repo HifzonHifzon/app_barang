57fe5e1261b6d7b39f0bc69eab433c8d0ee7c8ec
HifzonHifzon <Hifzon746@gmail.com>
2020-10-24 10:15:59 +0700

    finish

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -4,11 +4,16 @@ class Barang extends CI_Controller {
 
     function __construct() {
         parent::__construct();
+        
+        // load model Barang_model 
         $this->load->model('Barang_model');
     }
 
     function index() {
+        // get Data barang from model
         $data = $this->Barang_model->getData('tbl_barang');
+
+        // passing data to view
         $this->load->view('list',["list" => $data]);
     }
 }
--- application/models/Barang_model.php
+++ application/models/Barang_model.php
@@ -3,6 +3,7 @@
 
 class Barang_model extends CI_Model{
 
+    // process query data barang
     public function getData() {
         $data = $this->db->get('tbl_barang')->result();
         return $data;
